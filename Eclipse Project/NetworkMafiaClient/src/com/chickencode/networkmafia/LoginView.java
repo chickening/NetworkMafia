118c30c1288de9c93e6f4afed592ab1f9648c646
chickening <32235083+chickening@users.noreply.github.com>
2018-05-28 18:25:56 +0900

    add

---
+++ Eclipse Project/NetworkMafiaClient/src/com/chickencode/networkmafia/LoginView.java
@@ -0,0 +1,11 @@
+package com.chickencode.networkmafia;
+
+import javax.swing.JPanel;
+
+public class LoginView extends JPanel
+{
+	LoginView()
+	{
+		this.setBounds(0, 0, 540, 960);
+	}
+}
---
+++ Eclipse Project/NetworkMafiaClient/src/com/chickencode/networkmafia/MainFrame.java
@@ -0,0 +1,12 @@
+package com.chickencode.networkmafia;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame
+{
+	MainFrame()
+	{
+		this.setResizable(false);
+		this.setBounds(0,0,540,960);
+	}
+}
