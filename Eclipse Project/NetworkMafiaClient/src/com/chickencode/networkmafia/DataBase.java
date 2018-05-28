8d4dccc4cd8133aa9000eb1b67940e756fe37391
chickening <chickening182@gmail.com>
2018-05-28 20:48:10 +0900

    add loginView

---
+++ Eclipse Project/NetworkMafiaClient/src/com/chickencode/networkmafia/DataBase.java
@@ -0,0 +1,42 @@
+package com.chickencode.networkmafia;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.HashMap;
+
+public class DataBase 
+{
+	private HashMap<String , Image> imgData;
+	private static boolean haveInstance = false;
+	private static DataBase instance;
+	private  DataBase()
+	{
+		imgData = new HashMap<String , Image>();
+		init();
+	}
+	public static DataBase getDataBase()
+	{
+		if(!haveInstance)
+		{
+			haveInstance = false;
+			instance = new DataBase();
+		}
+		return instance;
+	}
+	void init()
+	{
+		addImage("img_logo","img//logo.png"); 
+		addImage("img_mafia","img//mafia.png");
+		addImage("img_key","img//key.png");
+		addImage("img_lock","img//lock.png");
+	}
+	void addImage(String name , String src)
+	{
+		Image img = Toolkit.getDefaultToolkit().getImage(src);
+		imgData.put(name, img);
+	}
+	Image getImage(String name)
+	{
+		return imgData.get(name);
+	}
+}
--- Eclipse Project/NetworkMafiaClient/src/com/chickencode/networkmafia/MainFrame.java
+++ Eclipse Project/NetworkMafiaClient/src/com/chickencode/networkmafia/MainFrame.java
@@ -1,12 +1,22 @@
 package com.chickencode.networkmafia;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame
 {
+	JPanel front;
 	MainFrame()
 	{
 		this.setResizable(false);
 		this.setBounds(0,0,540,960);
+		this.setLayout(null);
+		this.setVisible(true);
+		front = LoginView.getInstance();
+		this.add(front);
+		this.repaint();
 	}
 }
+ 
